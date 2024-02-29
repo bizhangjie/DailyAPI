db271dd2e2b2b31d9372fe019a11f2b42e0da544
zhangjie <768284730@qq.com>
2024-02-29 18:22:03 +0800

    18

--- py/pyAPI/api/zonghe.py
+++ py/pyAPI/api/zonghe.py
@@ -8,3 +8,14 @@
 # @Software: PyCharm
 # @ Description：I'm in charge of my Code
 # -------------------------------------------------
+
+
+import requests
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'
+}
+
+resp = requests.get('https://jable.tv/videos/jufe-124/',headers=headers)
+print(resp.text)
+
