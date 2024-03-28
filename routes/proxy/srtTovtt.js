54b55580b2777557dfbf6cf6dbf8b8e92151f465
zhangjie <768284730@qq.com>
2024-03-28 14:30:41 +0800

    添加vercel部署文件

--- routes/proxy/srtTovtt.js
+++ routes/proxy/srtTovtt.js
@@ -7,7 +7,7 @@ const response = require('../../utils/response');
 // 缓存键名
 const cacheKey = "srtTovttData";
 
-const Host = 'https://github.com/niucnet/jinricp/raw/main/GIF&%E5%AD%97%E5%B9%95&m3u8/';
+const Host = 'https://api.buxiangyao.link/jinricp(M3U8)/';
 
 // 播放地址
 proxyRouter.get("/srttovtt", async (ctx) => {
