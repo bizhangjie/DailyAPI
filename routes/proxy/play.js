9203471c3caf896462c933341796e79707540564
zhangjie <768284730@qq.com>
2024-06-09 23:49:15 +0800

    添加vercel部署文件

---
+++ routes/proxy/play.js
@@ -0,0 +1,49 @@
+const Router = require("koa-router");
+const proxyRouter = new Router();
+const axios = require("axios");
+const {get, set} = require("../../utils/cacheData");
+const response = require('../../utils/response');
+const cheerio = require('cheerio');
+
+// 缓存键名
+const cacheKey = "playData";
+
+// 播放地址
+proxyRouter.get("/kk", async (ctx) => {
+    const {url, pg} = ctx.query;
+    let zURl = url + pg;
+    const key = `${cacheKey}_${zURl}`;
+    try {
+        // 从缓存中获取数据
+        let data = await get(key);
+        if (!data) {
+            // 从服务器拉取数据
+            const sourceHtml = await axios.get(url);
+            const source = cheerio.load(sourceHtml.data);
+            const htmlContent = source.html().replace(/&amp;/g, "&").replace(/&quot;/g, '"');
+            let regex2 = /"video":{"url":"(.*?)"/g;
+            if (url.split("/")[2] === "hlj.fun") {
+                regex2 = /"url":"(.*?)"/g;
+            }
+            let urlList = [];
+            let match;
+            while ((match = regex2.exec(htmlContent)) !== null) {
+                const url = match[1].replace(/\\\//g, '/');
+                urlList.push(url);
+            }
+            data = urlList[pg - 1];
+            // 将数据写入缓存
+            await set(key, data);
+        }
+        // 重定向到获取的URL
+        ctx.redirect(data);
+    } catch (err) {
+        // 如果出现错误，返回错误信息
+        ctx.status = 606;
+        ctx.body = "此类数据有毒，但是很好看！";
+    }
+});
+
+
+
+module.exports = proxyRouter;
